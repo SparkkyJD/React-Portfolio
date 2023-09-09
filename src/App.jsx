7d68632e61ea0605b56f3a4994541e3818852cc0
SparkkyJD <wagnd4e@gmail.com>
2023-09-09 18:28:36 -0400

    adding animate.css and react awesome reveal. added bg image and intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import React from 'react';
 import Navbar from './components/Navbar';
+import Intro from './components/intro';
 
 function App() {
   return (
     
     <div className="app">
       <Navbar />
+      <Intro/>
     </div>
   );
 }
