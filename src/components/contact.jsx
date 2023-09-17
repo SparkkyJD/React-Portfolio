24e6269cc1450b057a2a62d2317b154bbc0b8f0b
SparkkyJD <wagnd4e@gmail.com>
2023-09-17 19:11:33 -0400

    fixing more netlify form errors

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,68 +1,69 @@
 import React, { useState } from 'react';
 
 const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
+    return Object.keys(data)
+        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&');
 };
 
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'hidden', ...formData }),
-    })
-      .then(() => {
-        // Replace this with your custom success handling
-        console.log('Form submitted successfully');
-      })
-      .catch((error) => {
-        // Handle errors here
-        console.error('Error submitting form:', error);
-      });
-  };
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({ 'form-name': 'hidden', ...formData }),
+        })
+            .then(() => {
+                console.log('Form submitted successfully');
+            })
+            .catch((error) => {
+                console.error('Error submitting form:', error);
+            });
+    };
 
-  return (
-    <div className="max-w-md mx-auto h-[75vh]">
-      <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
+    return (
+        <div className="max-w-md mx-auto h-[75vh]">
+            <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
 
-      <form name="hidden" method="POST" data-netlify="true" onSubmit={handleSubmit}>
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" value={formData.name} onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" value={formData.email} onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" value={formData.message} onChange={handleChange}></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
-  );
+            <form name="hidden" method="POST" data-netlify="true" action="/" onSubmit={handleSubmit}>
+                <p>
+                    <label>
+                        Your Name: <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your Email: <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <textarea name="message" value={formData.message} onChange={handleChange}></textarea>
+                    </label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </div>
+    );
 }
+
+
+
