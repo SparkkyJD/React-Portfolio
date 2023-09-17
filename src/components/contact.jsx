3c65ffdf08b8c387d44b57e711ec91ad39437c00
SparkkyJD <wagnd4e@gmail.com>
2023-09-17 18:40:09 -0400

    fixing netlify form handler

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -59,9 +59,21 @@ export default function Contact() {
     return (
         <div className="max-w-md mx-auto h-[75vh]">
             <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
-            <form onSubmit={handleSubmit} action="/send-email" method="POST" className="space-y-4 mx-8">
+
+            <form
+                onSubmit={handleSubmit}
+                action="/send-email"
+                method="POST"
+                className="space-y-4 mx-8"
+                data-netlify="true"
+            >
+
+                <input type="hidden" name="form-name" value="contact" />
+
                 <div className="flex flex-col">
-                    <label htmlFor="name" className="text-lg font-medium mb-1">Name:</label>
+                    <label htmlFor="name" className="text-lg font-medium mb-1">
+                        Name:
+                    </label>
                     <input
                         type="text"
                         id="name"
@@ -70,10 +82,14 @@ export default function Contact() {
                         onChange={handleChange}
                         className="px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
                     />
-                    {errors.name && <p className="text-red-500 mt-1">{errors.name}</p>}
+                    {errors.name && (
+                        <p className="text-red-500 mt-1">{errors.name}</p>
+                    )}
                 </div>
                 <div className="flex flex-col">
-                    <label htmlFor="email" className="text-lg font-medium mb-1">Email:</label>
+                    <label htmlFor="email" className="text-lg font-medium mb-1">
+                        Email:
+                    </label>
                     <input
                         type="email"
                         id="email"
@@ -82,10 +98,14 @@ export default function Contact() {
                         onChange={handleChange}
                         className="px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
                     />
-                    {errors.email && <p className="text-red-500 mt-1">{errors.email}</p>}
+                    {errors.email && (
+                        <p className="text-red-500 mt-1">{errors.email}</p>
+                    )}
                 </div>
                 <div className="flex flex-col">
-                    <label htmlFor="message" className="text-lg font-medium mb-1">Message:</label>
+                    <label htmlFor="message" className="text-lg font-medium mb-1">
+                        Message:
+                    </label>
                     <textarea
                         id="message"
                         name="message"
@@ -93,12 +113,22 @@ export default function Contact() {
                         onChange={handleChange}
                         className="px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-blue-500"
                     ></textarea>
-                    {errors.message && <p className="text-red-500 mt-1">{errors.message}</p>}
+                    {errors.message && (
+                        <p className="text-red-500 mt-1">{errors.message}</p>
+                    )}
                 </div>
-                <button type="submit" href="contact" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 
-            focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Submit
+                <button
+                    type="submit"
+                    href="contact"
+                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br 
+                    focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm 
+                    px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                    Submit
                 </button>
             </form>
+
+
         </div>
     );
 
