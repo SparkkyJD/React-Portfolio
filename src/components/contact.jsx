b75e7d3b1fe67dc268b99a9e3814ae810e004f2f
SparkkyJD <wagnd4e@gmail.com>
2023-09-17 20:08:33 -0400

    d

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 export default function Contact() {
@@ -7,12 +7,31 @@ export default function Contact() {
         <div className="max-w-md mx-auto h-[75vh]">
             <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
 
-            <form name="Contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="form-name" value="Contact" />
-                <input type="text" name="name" placeholder="Name" />
-                <input type="email" name="email" placeholder="Email" />
-                <textarea name="message" placeholder="Message"></textarea>
-                <div className="form-btn"><button type="submit">SUBMIT</button></div>
+            <form
+                name="contact v1"
+                method="post"
+                data-netlify="true"
+                onSubmit="submit"
+            >
+                <input type="hidden" name="form-name" value="contact v1" />
+
+                <div>
+                    <label>First name <br />
+                        <input type="text" name="first-name" />
+                    </label>
+                </div>
+
+                <div>
+                    <label htmlFor="email">Email</label> <br />
+                    <input id="email" type="email" name="email"/>
+                </div>
+
+                <div>
+                    <label>Messageewe <br />
+                      <textarea name="comments"></textarea>
+                    </label>
+                </div>
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
