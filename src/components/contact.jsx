cd853ba29f256761cbdd959235a71ae8d85dc80b
SparkkyJD <wagnd4e@gmail.com>
2023-09-17 20:29:53 -0400

    f

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <div className="max-w-md mx-auto h-[75vh]">
       <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
 
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/">
         <div>
           <label>First name <br />
             <input type="text" name="first-name" />
