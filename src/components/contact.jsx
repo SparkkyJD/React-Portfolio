21b159a48b49b49d02c9724db89f2b0cb37d40e7
eric wagner <wagnd4e@gmail.com>
2023-09-18 12:32:12 -0400

    t

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
 export default function Contact() {
-  const handleSubmit = (e) => {
-    e.preventDefault(); 
-  };
-
   return (
     <div className="max-w-md mx-auto h-[75vh]">
       <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
 
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
-      <div>
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/">
+        <div>
           <label>First name <br />
             <input type="text" name="first-name" />
           </label>
@@ -23,7 +19,7 @@ export default function Contact() {
 
         <div>
           <label>Message <br />
-            <textarea name="comments"></textarea>
+            <textarea name="comments"></textarea>zzz
           </label>
         </div>
 
