4045a5371f24c07473c13ce6a1fd15d7173a4781
eric wagner <wagnd4e@gmail.com>
2023-09-18 13:06:28 -0400

    l

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function Contact() {
   return (
     <div className="max-w-md mx-auto h-[75vh]">
-      <h2 className="text-5xl font-semibold m-20 flex justify-center">Contact Me</h2>
-
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <h2 className="text-5xl font-semibold m-20 flex justify-center">
+        Contact Me
+      </h2>
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
         <div>
-          <label>First name <br />
+          <label>
+            First name <br />
             <input type="text" name="first-name" />
           </label>
         </div>
@@ -18,7 +21,8 @@ export default function Contact() {
         </div>
 
         <div>
-          <label>Message <br />
+          <label>
+            Message <br />
             <textarea name="comments"></textarea>
           </label>
         </div>
